01bbf78c554e82667ad5db5d57bef55e5ed48d11
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-09-11 13:54:05 -0500

    [add] getProducts Completed

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
-const { getProductsBD, getCategoriesBD } = require('../models')
+const { getProductsBD, getCategoriesBD } = require('../models');
+const { Op } = require('sequelize');
 
 async function getCategories(req, res){
   try {
@@ -15,11 +16,46 @@ async function getCategories(req, res){
   }
 }
 
-function getProducts(req, res){
-  const rpta = getProductsBD();
-  res.json({
-    rpta
-  });
+async function getProducts(req, res){
+  try {
+    
+    const { 
+      text,
+      page,
+      limit,
+      order_by,
+      order_direction
+    } = req.query;
+
+    let search = {}, order = [];
+
+    if(text){
+      search = {
+        where: {
+          name: {
+            [Op.like]: `%${text}%` 
+          }
+        }
+      };
+    }
+
+    if(order_by && order_direction){
+      order.push(order_by, order_direction);
+    }
+
+    const products = await getProductsBD( page, limit, search, order);
+
+    return res.status(200).send({
+      status: "success",
+      data: products
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      status: "error",
+      error
+    });
+  }
 }
 
 
--- models/index.js
+++ models/index.js
@@ -1,4 +1,6 @@
 const { Category } = require("./category");
+const { Product } = require("./product");
+const { getOffset, getNextPage, getPreviousPage } = require('../helpers');
 
 async function getCategoriesBD(){
   try {
@@ -9,12 +11,38 @@ async function getCategoriesBD(){
   }
 }
 
-function getProductsBD(){
-  return{
-    msg: "Mensaje desde el model"
+async function getProductsBD(pageSize, pageLimit, search = {}, order = []){
+  
+  try {
+    const limit = parseInt(pageLimit, 10) || 12;
+    const page = parseInt(pageSize, 10) || 1;
+
+    let options = {
+        offset: getOffset(page, limit),
+        limit: limit,
+    };
+
+    if (Object.keys(search).length) {
+        options = {options, ...search};
+    }
+
+    if (order && order.length) {
+        options['order'] = [order];
+    }
+
+    let { count, rows } = await Product.findAndCountAll(options);
+
+    return {
+        previousPage: getPreviousPage(page),
+        currentPage: page,
+        nextPage: getNextPage(page, limit, count),
+        total: count,
+        limit: limit,
+        data: rows
+    }
+  } catch (error) {
+    throw new Error(error);
   }
 }
-
-
 
 module.exports = { getProductsBD, getCategoriesBD }
