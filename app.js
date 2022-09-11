fc83743c208806c61dbcd31c95238d644b23abc0
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-09-11 01:32:15 -0500

    [add] Test connection BD

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
-const {json, urlencoded} = require('body-parser')
+const {json, urlencoded} = require('body-parser');
+const { connection } = require('./database/db');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(json());
 app.use(urlencoded({ extended: true }));
+
+(async() => {
+  try {
+    await connection.authenticate();
+    console.log('Connect to database successfully! 🚀');
+  } catch (error) {
+    throw new Error(error);
+  }
+})()
+
 
 app.listen(PORT,() => {
   console.log('Server running to port:'+ PORT);
