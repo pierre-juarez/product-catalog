8a463831261901b7ee9e4466ec12c2057123f3a2
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2022-09-12 20:16:51 -0500

    [add] Deploy frontend structure

--- backend/database/db.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-// Credentials for connecting to the database
-const connection = new Sequelize('bsale_test', 'bsale_test', 'bsale_test', {
-  host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
-  dialect: 'mysql'
-});
-
-module.exports = { connection }
--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const api = express.Router();
-const controller = require('../controllers');
-
-api.get('/getProducts',controller.getProducts);
-api.get('/getCategories', controller.getCategories);
-
-module.exports = { api }
